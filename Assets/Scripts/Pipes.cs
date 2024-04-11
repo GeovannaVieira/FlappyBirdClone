2b0afd69e151bfe3408ab154e8f4246a134c8a97
GeovannaVieira <110910182+GeovannaVieira@users.noreply.github.com>
2024-04-10 21:38:42 -0300

    Add files via upload

---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private void Update()
+    {
+        transform.position += speed
+            * Time.deltaTime * Vector3.left;
+
+    }
+}
---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject prefab;
+    public float maxHeight;
+    public float minHeight;
+    public float maxTime = .5f;
+
+    private float _timer;
+
+   private void Start()
+    {
+        InstantiateObject();
+    }
+
+    private void Update()
+    {
+        if (_timer > maxTime) {
+            InstantiateObject();
+            _timer = 0;
+        }
+
+
+        _timer += Time.deltaTime;
+    }
+
+    private void InstantiateObject()
+    {
+        var instance = Instantiate(prefab);
+        var y = Random.Range(minHeight, maxHeight);
+        instance.transform.position
+            = transform.position + new Vector3(0, y);
+
+        Destroy(instance, 10f);
+    }
+}
